7cdbdc45547c776866f5b4b534c609eb4c9f54bd
lexuanhuy <Lexuanhuy8695@gmail.com>
2022-10-08 10:19:21 +0700

    navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import "./App.scss";
-
+import Header from "./components/header/header";
 const App = () => {
-  return <div className="App-container">hello huy</div>;
+  return (
+    <div className="App-container">
+      <Header />
+    </div>
+  );
 };
 export default App;
