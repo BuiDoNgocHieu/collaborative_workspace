2c84183b97f243a4ccda8ee5e05b8578569f2966
lexuanhuy <Lexuanhuy8695@gmail.com>
2022-10-08 10:58:07 +0700

    react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import "./App.scss";
 import Header from "./components/header/header";
+import { Link } from "react-router-dom";
+
 const App = () => {
   return (
     <div className="App-container">
       <Header />
+      <div>phan than</div>
+      <button>
+        <Link to="./users"> den trang user</Link>
+      </button>
+      <button>
+        <Link to="./admin"> ve trang Admin</Link>
+      </button>
     </div>
   );
 };
