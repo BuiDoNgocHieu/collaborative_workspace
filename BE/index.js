8746944fc1f5e4a2b821508f18dbb3cde558358b
sonhoang205 <hson20052001@gmail.com>
2022-10-17 21:37:42 +0700

    BE initial source code

---
+++ BE/index.js
@@ -0,0 +1,33 @@
+require('dotenv').config()
+const express = require('express');
+
+const mongoose = require('mongoose');
+
+
+mongoose.connect(process.env.MONGODB_URI, err => {
+  if (err) {
+    return console.log('DB connect err', err);
+  }
+  console.log('DB connect success');
+});
+
+const app = express();
+app.use(express.json());
+
+
+
+app.use('*', (req, res) => {
+  res.send({ message: '404 not found' })
+})
+
+app.use((err,req,res,next)=>{
+  console.error(err.stack);
+  res.status.send({success:0,message:err.message})
+})
+
+app.listen(process.env.PORT || 6060, (err) => {
+  if (err) {
+    return console.log('Server Error', err);
+  }
+  console.log('Server started');
+})
